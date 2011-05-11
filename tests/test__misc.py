dfe05412942f09404da334b2095cdf448e618f5f
Rotem Yaari <vmalloc@gmail.com>
2011-05-11 16:54:16 +0300

    Add tests for backwards compatibility

--- tests/test__misc.py
+++ tests/test__misc.py
@@ -9,3 +9,27 @@ class TestImportsFromUnittestStillWork(unittest.TestCase):
             except ImportError:
                 self.fail("Name {!r} is not importable!".format(name))
 
+
+class TestBackwardCompatibility(unittest.TestCase):
+    def test__is_backwards_compatible(self):
+        from infi.unittest import TestCase
+        expected = ['a', 'b', 'c']
+        run = []
+        class SampleTest(TestCase):
+            def test__a(self):
+                run.append('a')
+            def test__b(self):
+                run.append('b')
+            def test__c(self):
+                run.append('c')
+        suite = unittest.TestLoader().loadTestsFromTestCase(SampleTest)
+        result = unittest.TestResult()
+        suite.run(result)
+        self.assertEquals(result.failures, [])
+        self.assertEquals(result.errors, [])
+        self.assertEquals(result.testsRun, 3)
+        self.assertEquals(set(run), set(expected))
+    def test__is_subclass_of_unittest_testcase(self):
+        from infi.unittest import TestCase
+        self.assertTrue(issubclass(TestCase, unittest.TestCase))
+
