555b45f676f4304fb875e542b1d0c2099bd6ce57
Rotem Yaari <vmalloc@gmail.com>
2011-07-17 16:31:34 +0300

    Add tests

--- tests/test__misc.py
+++ tests/test__misc.py
@@ -1,4 +1,7 @@
 import unittest
+from infi.unittest import TestCase as InfiTestCase
+from infi.unittest import TestLoader as InfiTestLoader
+from infi.unittest import parameters
 from test_utils import run_suite_assert_success
 
 class TestImportsFromUnittestStillWork(unittest.TestCase):
@@ -13,12 +16,11 @@ class TestImportsFromUnittestStillWork(unittest.TestCase):
 
 class TestBackwardCompatibility(unittest.TestCase):
     def test__is_backwards_compatible(self):
-        from infi.unittest import TestCase
         expected = reduce(list.__add__,
                           (['setup', x, 'teardown'] for x in 'abc'),
                           [])
         run = []
-        class SampleTest(TestCase):
+        class SampleTest(InfiTestCase):
             def setUp(self):
                 super(SampleTest, self).setUp()
                 run.append('setup')
@@ -36,4 +38,23 @@ class TestBackwardCompatibility(unittest.TestCase):
     def test__is_subclass_of_unittest_testcase(self):
         from infi.unittest import TestCase
         self.assertTrue(issubclass(TestCase, unittest.TestCase))
+
+class RepresentationTest(unittest.TestCase):
+    def test__represnetation(self):
+        class DummyTest(InfiTestCase):
+            @parameters.iterate('x', [1, 2, 3])
+            def setUp(self, x):
+                super(DummyTest, self).setUp()
+                self.x = x
+            @parameters.iterate('y', [1, 2, 3])
+            def test__something(self, y):
+                raise NotImplementedError() # pragma: no cover
+        loader = InfiTestLoader()
+        suite = loader.loadTestsFromTestCase(DummyTest)
+        self.assertEquals(suite.countTestCases(), 9)
+        for case in suite:
+            self.assertIsInstance(str(case), basestring)
+            self.assertIsInstance(repr(case), basestring)
+            import pdb
+            pdb.set_trace()
 
